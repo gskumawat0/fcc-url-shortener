4800acd2965369237588b202994bd56ee18f28ce
Glitch (denim-debt) <none>
2019-03-15 05:46:25 +0000

    🌌👩‍👩‍👦 Checkpoint ./server.js:3298818/153

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -31,8 +32,9 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.post('/api/shorturl/new', function(req, res){
-  console.log(req.body);
+app.post('/api/shorturl/new', async function(req, res){
+  let url = req.body.url;
+  let isValidUrl = await dns.lookup(url)
   res.redirect('/');
 })
 
