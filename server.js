9648b2fb64daeb60e6f9bb0d2b40c4947145e823
Glitch (denim-debt) <none>
2019-03-15 05:36:24 +0000

    🏣🦄 Checkpoint ./server.js:3298818/236

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-
+var bodyParser = require('body-parser');
 var cors = require('cors');
 
 var app = express();
@@ -18,7 +18,7 @@ app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
-
+app.use(bodyParser.json())
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function(req, res){
@@ -30,6 +30,11 @@ app.get('/', function(req, res){
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
+app.post('/api/shorturl/new', function(req, res){
+  console.log(req.body);
+  res.redirect('/');
+})
 
 
 app.listen(port, function () {
